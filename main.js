de38cd7ede46142f3ebcce40ec03a731c9dc5a3d
Ryan Graham <r.m.graham@gmail.com>
2012-06-04 20:48:29 -0700

    Add a web server for accepting commands

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
 var chroot = require("./chroot.js");
+var http = require('http');
+var readFile = require('fs').readFile;
+var qs = require('querystring');
 
-chroot.run("ls", function (ecode, out, err) {
-    console.log("Ran 'ls', exit code: " + ecode + "\nSTDOUT:\n " + out + "\nSTDERR:\n" + err);
-});
+//chroot.run("ls", function (ecode, out, err) {
+//    console.log("Ran 'ls', exit code: " + ecode + "\nSTDOUT:\n " + out + "\nSTDERR:\n" + err);
+//});
+
+function cmdRequestHandler(request, response) {
+    //console.log(request);
+    if (request.method == 'GET') {
+        readFile('assets/cmd.html', function(err, data) {
+            if (err) {
+                response.statusCode = 400;
+                response.end(err);
+            } else {
+                response.end(data);
+            }
+        });
+        response.setHeader('Content-Type', 'text/html');
+        response.statusCode =  200;
+    } else {
+        request.on('data', function(data) {
+            var params = qs.parse(data.toString());
+            //			console.log(data);
+            console.log(params);
+            chroot.run(params.command, function (ecode, out, err) {
+                var body = "Ran '" + params.command + "', exit code: <pre>" + ecode + "</pre>\nSTDOUT:\n<pre>" + out + "</pre>\nSTDERR:\n<pre>" + err + "</pre>";
+                response.setHeader('Content-Type', 'text/html');
+                response.statusCode = ecode ? 400 : 200;
+                response.end(body);
+            });
+        });
+    }
+}
+
+var server = http.createServer(cmdRequestHandler);
+
+server.listen(8000);
