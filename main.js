df71e8ae00a905b0ce50a7446421fca29b7b24b3
Ryan Graham <r.m.graham@gmail.com>
2012-06-05 19:59:55 -0700

    Remove cruft

--- main.js
+++ main.js
@@ -1,45 +1,13 @@
 var chroot = require("./chroot.js");
 var web = require("./web.js");
-//chroot.run("ls", function (ecode, out, err) {
-//    console.log("Ran 'ls', exit code: " + ecode + "\nSTDOUT:\n " + out + "\nSTDERR:\n" + err);
-//});
 
 var server = new web.WWWCommandSource();
 
-server.on("command", function (cmd, reply) {
-    console.log("Comand from interwebs: " + cmd);
-        chroot.run(cmd, reply);
-});
+function runCommand(cmd, result_handler) {
+    console.log("Command from interwebs: " + cmd);
+    chroot.run(cmd, result_handler);
+}
 
-//
-//function cmdRequestHandler(request, response) {
-//    //console.log(request);
-//    if (request.method == 'GET') {
-//        readFile('assets/cmd.html', function(err, data) {
-//            if (err) {
-//                response.statusCode = 400;
-//                response.end(err);
-//            } else {
-//                response.end(data);
-//            }
-//        });
-//        response.setHeader('Content-Type', 'text/html');
-//        response.statusCode =  200;
-//    } else {
-//        request.on('data', function(data) {
-//            var params = qs.parse(data.toString());
-//            //			console.log(data);
-//            console.log(params);
-//            chroot.run(params.command, function (ecode, out, err) {
-//                var body = "Ran '" + params.command + "', exit code: <pre>" + ecode + "</pre>\nSTDOUT:\n<pre>" + out + "</pre>\nSTDERR:\n<pre>" + err + "</pre>";
-//                response.setHeader('Content-Type', 'text/html');
-//                response.statusCode = ecode ? 400 : 200;
-//                response.end(body);
-//            });
-//        });
-//    }
-//}
-//
-//var server = http.createServer(cmdRequestHandler);
+server.on("command", runCommand);
 
 server.listen(8000);
