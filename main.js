d7acb42c7964d5bc38d6dc5f35452b480b818418
Ryan Graham <r.m.graham@gmail.com>
2012-06-05 17:46:57 -0700

    First stab at extracting www based command reader
    Doesn't work

--- main.js
+++ main.js
@@ -1,40 +1,45 @@
 var chroot = require("./chroot.js");
-var http = require('http');
-var readFile = require('fs').readFile;
-var qs = require('querystring');
-
+var web = require("./web.js");
 //chroot.run("ls", function (ecode, out, err) {
 //    console.log("Ran 'ls', exit code: " + ecode + "\nSTDOUT:\n " + out + "\nSTDERR:\n" + err);
 //});
 
-function cmdRequestHandler(request, response) {
-    //console.log(request);
-    if (request.method == 'GET') {
-        readFile('assets/cmd.html', function(err, data) {
-            if (err) {
-                response.statusCode = 400;
-                response.end(err);
-            } else {
-                response.end(data);
-            }
-        });
-        response.setHeader('Content-Type', 'text/html');
-        response.statusCode =  200;
-    } else {
-        request.on('data', function(data) {
-            var params = qs.parse(data.toString());
-            //			console.log(data);
-            console.log(params);
-            chroot.run(params.command, function (ecode, out, err) {
-                var body = "Ran '" + params.command + "', exit code: <pre>" + ecode + "</pre>\nSTDOUT:\n<pre>" + out + "</pre>\nSTDERR:\n<pre>" + err + "</pre>";
-                response.setHeader('Content-Type', 'text/html');
-                response.statusCode = ecode ? 400 : 200;
-                response.end(body);
-            });
-        });
-    }
-}
+var server = new web.WWWCommandSource();
 
-var server = http.createServer(cmdRequestHandler);
+server.on("command", function (cmd, reply) {
+    console.log("Comand from interwebs: " + cmd);
+        chroot.run(cmd, reply);
+});
+
+//
+//function cmdRequestHandler(request, response) {
+//    //console.log(request);
+//    if (request.method == 'GET') {
+//        readFile('assets/cmd.html', function(err, data) {
+//            if (err) {
+//                response.statusCode = 400;
+//                response.end(err);
+//            } else {
+//                response.end(data);
+//            }
+//        });
+//        response.setHeader('Content-Type', 'text/html');
+//        response.statusCode =  200;
+//    } else {
+//        request.on('data', function(data) {
+//            var params = qs.parse(data.toString());
+//            //			console.log(data);
+//            console.log(params);
+//            chroot.run(params.command, function (ecode, out, err) {
+//                var body = "Ran '" + params.command + "', exit code: <pre>" + ecode + "</pre>\nSTDOUT:\n<pre>" + out + "</pre>\nSTDERR:\n<pre>" + err + "</pre>";
+//                response.setHeader('Content-Type', 'text/html');
+//                response.statusCode = ecode ? 400 : 200;
+//                response.end(body);
+//            });
+//        });
+//    }
+//}
+//
+//var server = http.createServer(cmdRequestHandler);
 
 server.listen(8000);
--- web.js
+++ web.js
@@ -1,4 +1,54 @@
 var http = require('http');
+var util = require('util');
+var events = require('events');
+var qs = require('querystring');
+var readFile = require('fs').readFile;
+var http = require('http');
 
-exports.listner = function () {
+
+function WWWCommandSource() {
+    events.EventEmitter.call(this);
+    this.server = http.createServer(this.requestHandler);
+}
+
+util.inherits(WWWCommandSource, events.EventEmitter);
+
+WWWCommandSource.prototype.listen = function (port) {
+    this.server.listen(port);
 };
+
+function commandResponseWrapper(command, httpResponse) {
+    return function (ecode, out, err) {
+        var body = "Ran '" + command + "', exit code: <pre>" + ecode + "</pre>\nSTDOUT:\n<pre>" + out + "</pre>\nSTDERR:\n<pre>" + err + "</pre>";
+        httpResponse.setHeader('Content-Type', 'text/html');
+        httpResponse.statusCode = ecode ? 400 : 200;
+        httpResponse.end(body);
+    };
+}
+
+WWWCommandSource.prototype.requestHandler = function (request, response) {
+    //console.log(request);
+    if (request.method == 'GET') {
+        readFile('assets/cmd.html', function(err, data) {
+            if (err) {
+                response.statusCode = 400;
+                response.end(err);
+            } else {
+                response.end(data);
+            }
+        });
+        response.setHeader('Content-Type', 'text/html');
+        response.statusCode =  200;
+    } else {
+        request.on('data', function(data) {
+            var params = qs.parse(data.toString());
+            //			console.log(data);
+            console.log(params);
+            this.emit("command",
+                        params.command,
+                        commandResponseWrapper(params.command, response));
+        });
+    }
+};
+
+exports.WWWCommandSource = WWWCommandSource;
