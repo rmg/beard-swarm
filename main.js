a5156df1948b0045f0ced9148d127ce334d16559
Ryan Graham <r.m.graham@gmail.com>
2012-09-21 10:25:53 -0700

    Coding style

--- main.js
+++ main.js
@@ -1,45 +1,44 @@
-var chroot = require("./chroot.js");
-var web = require("./web.js");
-var events = require("events");
-var util = require('util')
-
-var server = new web.WWWCommandSource();
+var chroot = require("./chroot.js")
+  , web = require("./web.js")
+  , events = require("events")
+  , util = require('util')
+  , server = new web.WWWCommandSource();
 
 function runCommand(cmd, result_handler) {
-  console.log("Command from interwebs: " + cmd);
-  chroot.run(cmd, result_handler);
+  console.log("Command from interwebs: " + cmd)
+  chroot.run(cmd, result_handler)
 }
 
-var fake_emitter = new events.EventEmitter();
+var fake_emitter = new events.EventEmitter()
 
 function fake_task() {
-  var nums = [1,1];
+  var nums = [1,1]
   function fib() {
-    var next = nums[0] + nums[1];
-    fake_emitter.emit('data', nums[0]);
-    nums[0] = nums[1];
-    nums[1] = next;
+    var next = nums[0] + nums[1]
+    fake_emitter.emit('data', nums[0])
+    nums[0] = nums[1]
+    nums[1] = next
   }
-  setInterval(fib, 500);
+  setInterval(fib, 500)
 }
 
 function fake_logger(data) {
-  console.log(data);
+  console.log(data)
 }
 function turn_on() {
-  fake_emitter.on("data", fake_logger);
+  fake_emitter.on("data", fake_logger)
 }
 function turn_off() {
-  fake_emitter.removeListener('data', fake_logger);
+  fake_emitter.removeListener('data', fake_logger)
 }
 
-setTimeout(turn_on, 50);
-setTimeout(turn_off, 1500);
-setTimeout(turn_on, 2500);
-//fake_task();
+setTimeout(turn_on, 50)
+setTimeout(turn_off, 1500)
+setTimeout(turn_on, 2500)
+//fake_task()
 
-server.on("command", runCommand);
+server.on("command", runCommand)
 
-server.listen(8000);
+server.listen(8000)
 
 //runCommand("gcc --version", util.inspect)
