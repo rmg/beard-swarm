034d4ef0da4f6820b4915fb7b381336b2268444e
Ryan Graham <r.m.graham@gmail.com>
2013-04-01 15:37:49 -0700

    oops.

--- test/sanity.js
+++ test/sanity.js
@@ -3,7 +3,7 @@ var fs     = require('fs')
 
 describe("Testing environment sanity check", function() {
     it("Has a chroot-able environment in ./chroot", function() {
-        assert(fs.existsSync('./chroots'),
+        assert(fs.existsSync('./chroot'),
                "You need a chroot environment at ./chroot to run tests, try untarring a Gentoo stage3 into ./chroot")
     })
 })
