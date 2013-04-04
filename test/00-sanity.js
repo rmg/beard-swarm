fd61739f1730338018f5c062500319586f05641e
Ryan Graham <r.m.graham@gmail.com>
2013-04-04 15:16:44 -0700

    Make sanity test fully async, for no reason

--- test/00-sanity.js
+++ test/00-sanity.js
@@ -2,8 +2,11 @@ var fs     = require('fs')
   , assert = require('assert')
 
 describe("Testing environment sanity check", function() {
-    it("Has a chroot-able environment in ./chroot", function() {
-        assert(fs.existsSync('./chroot'),
-               "You need a chroot environment at ./chroot to run tests, try untarring a Gentoo stage3 into ./chroot")
-    })
+  it("Has a chroot-able environment in ./chroot", function(done) {
+    function completer(exists) {
+        assert(exists, "You need a chroot environment at ./chroot to run tests, try untarring a Gentoo stage3 into ./chroot")
+        done()
+    }
+    fs.exists('./chroot', completer)
+  })
 })
